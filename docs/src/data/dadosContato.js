561e8ae79d13f5d2b72e793828c0dfd4c2df1876
Henrique Bueno <buenohenrique312007@gmail.com>
2026-09-24 16:08:44 -0300

    Update: LinkedIn link in dadosContato.js

--- docs/src/data/dadosContato.js
+++ docs/src/data/dadosContato.js
@@ -1,4 +1,4 @@
 export const linksSociais = [
   { tipo: 'github', rotulo: 'GitHub', valor: 'github.com/Henri-Bueno', endereco: 'https://github.com/Henri-Bueno' },
-  { tipo: 'linkedin', rotulo: 'LinkedIn', valor: 'linkedin.com/in/henriquebueno', endereco: '#' }
+  { tipo: 'linkedin', rotulo: 'LinkedIn', valor: 'linkedin.com/in/henrique-m-bueno', endereco: 'https://www.linkedin.com/in/henrique-m-bueno/' }
 ];
